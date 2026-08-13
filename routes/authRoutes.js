0b07dc4a10cd40a5603f55b2748de41c0472474b
shivamkumar2005-abes <shivamprajapati1704@gmail.com>
2026-08-14 01:39:55 +0530

    Remove OTP verification, fix authRoutes casing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUsers, verifyOtp, resendOtp } = require("../controllers/authController");
+const { registerUser, loginUser, getUsers } = require("../controllers/authController");
 const { Protect } = require("../middleware/authMiddleware");
 const { admin } = require("../middleware/adminMiddleware");
 
 router.post("/register", registerUser);
-router.post("/verify-otp", verifyOtp);
-router.post("/resend-otp", resendOtp);
 router.post("/login", loginUser);
 router.get("/user", Protect, admin, getUsers);
 
